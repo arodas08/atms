c2b1e774debaa61da111ef45d85bbd536c39305d
arodas08 <adanrodas08@gmail.com>
2024-08-28 11:32:29 -0400

    actual

--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-function buscarEventos() {
-    const apiUrl = 'http://192.168.5.53:8080/api/eventos/?format=json';
-  
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        const eventList = document.getElementById('event-list');
-        eventList.innerHTML = ''; 
-        data.forEach(event => {
-          const listItem = document.createElement('li');
-          listItem.textContent = event.title; 
-          eventList.appendChild(listItem);
-        });
-        document.getElementById('modal').style.display = 'block';
-      })
-      .catch(error => {
-        console.error('Error fetching events:', error);
-      });
-  }
+const URL = 'https://api.thecatapi.com/v1/images/search';
+async function getURL () {
+    const response = await fetch(URL);
+    const data = await response.json();
+    const img = document.querySelector('img');
+    img.src = data[0].url;
+}
+
+getURL();
